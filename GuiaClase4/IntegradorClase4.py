9f69ae4b14c21dfb728ef9e33e8b945f821ba029
Benjamin Tourn <b.tourn.86@gmail.com>
2022-04-26 17:14:51 -0300

    Actualice ejercicio integrador de Clase 4

--- GuiaClase4/IntegradorClase4.py
+++ GuiaClase4/IntegradorClase4.py
@@ -5,6 +5,8 @@ import numpy as np
 ## ITEM 1: Genero un dataset con la misma funcion provista en clase
 # Función a utilizar: y = |x^2-4|
 from data import Data
+
+# Genero todos los datos de una sola vez
 n_samples = 2000
 dataset = Data(n_samples)
 
@@ -33,13 +35,15 @@ Y_pred_train = []
 Y_pred_test = []
 
 # Defino cantidad de regresiones a computar para cada una de las particiones previamente definidas
-n_regresiones = 20
-regresiones = []
+n_regresiones = 30
+regresiones_train = []
+regresiones_test = []
 
 # Defino arreglo para almacenar los valores de MSE de cada modelo
 mse = MSE()
 mse_train = np.zeros((n_regresiones, n_variantes))
 mse_test = np.zeros((n_regresiones, n_variantes))
+
 
 for i in range(n_variantes):
     
@@ -49,24 +53,33 @@ for i in range(n_variantes):
     YY_train.append(y_train)
     XX_test.append(x_test)
     YY_test.append(y_test)
+    Y_pred_train_i = []
+    Y_pred_test_i = []
+    regresiones_train_i = []
+    regresiones_test_i = []
     
     for j in range(n_regresiones):
         
-        # Entrenamiento de modelos de regresion
-        w_j = np.polyfit(x_train, y_train, j)
-        regresiones.append(w_j)
+        # Entrenamiento de modelos de regresion usando conjunto de train
+        w_train_j = np.polyfit(x_train, y_train, j)
+        regresiones_train_i.append(w_train_j)
         
         # Predicciones sobre los conjuntos de train y test para el modelo entrenado
-        y_pred_train_j = f_hat(x_train, w_j)
-        Y_pred_train.append(y_pred_train_j)
-        y_pred_test_j = f_hat(x_test, w_j)
-        Y_pred_test.append(y_pred_test_j)
-        
+        y_pred_train_j = f_hat(x_train, w_train_j)
+        Y_pred_train_i.append(y_pred_train_j)
+        y_pred_test_j = f_hat(x_test, w_train_j)
+        Y_pred_test_i.append(y_pred_test_j)
+            
         # Computo errores de train y test
         mse_train_ij = mse(y_train, y_pred_train_j)
         mse_test_ij = mse(y_test, y_pred_test_j)
         mse_train[j][i] = mse_train_ij
         mse_test[j][i] = mse_test_ij
+
+    Y_pred_train.append(Y_pred_train_i)
+    Y_pred_test.append(Y_pred_test_i)
+    regresiones_train.append(regresiones_train_i)
+    regresiones_test.append(regresiones_test_i)
 
 
 ## ITEM 3: Obtengo resultados de los modelos entrenados
@@ -91,11 +104,42 @@ tabla_mse_test.index = filas
 print(tabla_mse_test)
 
 
-# Minimo MSE
+
+# Minimo MSE de train y test
 min_MSE_train = np.min(mse_train)
 min_MSE_test = np.min(mse_test)
-min_ij = np.where(mse_test==min_MSE_test)
-print(f"El mínimo valor de MSE es {min_MSE_test:.4f}, obtenido con la partición N°{min_ij[1][0]:d} (seed={rand_seeds[min_ij[1][0]]:d}) \n y con un polinomio de grado {min_ij[0][0]:d}")
+min_train_ij = np.where(mse_train==min_MSE_train)
+min_test_ij = np.where(mse_test==min_MSE_test)
+print(f"El mínimo valor de MSE en test es {min_MSE_train:.4f}, obtenido con la partición N°{min_train_ij[1][0]:d} (seed={rand_seeds[min_train_ij[1][0]]:d}) \n y con un polinomio de grado {min_train_ij[0][0]:d}")
+print(f"El mínimo valor de MSE en test es {min_MSE_test:.4f}, obtenido con la partición N°{min_test_ij[1][0]:d} (seed={rand_seeds[min_test_ij[1][0]]:d}) \n y con un polinomio de grado {min_test_ij[0][0]:d}")
 
 # Ploteo de gráficas
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+# Graficas para conjunto de train, en la variante donde ocurrió el menor error
+variante = min_train_ij[1][0]
+regresion = min_train_ij[0][0]
+fig, axs = plt.subplots(1, sharey = True, figsize=(15, 9))
+axs.plot(XX_train[variante], YY_train[variante], ".", color = "Black")
+colores = list(mcolors.CSS4_COLORS.keys())
+x_range = np.linspace(-4, 4, n_samples)
+
+for i in range(n_regresiones):
+    axs.plot(x_range, f_hat(x_range, regresiones_train[variante][i]), linewidth=3.0, color=colores[i], label='Regresion Grado '+str(i))
+    axs.legend()
+
+
+# Graficas de error de train y test
+fig, axs = plt.subplots(1, 2, sharey = True, figsize=(15, 9))
+variante = min_train_ij[1][0]
+regresion = min_train_ij[0][0]
+axs[0].plot(np.arange(n_regresiones), mse_train[:, variante], linewidth=3.0, color = "Black")
+axs[0].axvline(x = regresion)
+axs[0].set_title('Error en conjunto de train')
+variante = min_test_ij[1][0]
+regresion = min_test_ij[0][0]
+axs[1].plot(np.arange(n_regresiones), mse_test[:, variante], linewidth=3.0, color = "Black")
+axs[1].axvline(x = regresion)
+axs[1].set_title('Error en conjunto de test')
+
--- GuiaClase4/data.py
+++ GuiaClase4/data.py
@@ -9,12 +9,12 @@ class Data(object):
         
     @staticmethod
     def _analytic_function(X):
-        return  np.abs(X**2-4)
+        return (np.abs(X**2-5)) + np.sin(2*X)*np.cos(X**3)
         
 
     def build_dataset(self, n):
         sigma_epsilon = 1
-        x_max = 6
+        x_max = 4
         x = x_max * (2 * np.random.rand(n) - 1)
         epsilon = sigma_epsilon * np.random.randn(n)
         y = self._analytic_function(x)
@@ -42,10 +42,10 @@ class Data(object):
     
     def plot_original(self):
         plt.figure(figsize=(12, 6))
-        x_range = np.linspace(-6, 6, 1000)
+        x_range = np.linspace(-4, 4, 1000)
         plt.scatter(self.X, self.y)
         plt.plot(x_range, self._analytic_function(x_range), 'r', linewidth=3.0)
         plt.xlabel('x', size=12)
         plt.ylabel('y', size=12)
-        plt.xticks(np.arange(-2, 6 + 1))
+        plt.xticks(np.arange(-4-1, 4 + 1))
         plt.show()
